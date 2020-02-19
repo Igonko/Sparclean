db381a3a8ed5335c73c724e6bc4b9e6e8331f153
Fedorenko Igor <axiom_ua@yahoo.com>
2020-02-20 01:59:10 +0200

    responsive first part

--- build/js/script.js
+++ build/js/script.js
@@ -1,35 +1,59 @@
 $('.estimator_small_option, .estimator_big_option').on('click', function (e) {
     $(e.target).addClass('active').siblings().removeClass('active');
     if($('.bedrooms_options.active').data('num') !== undefined && $('.bathrooms_options.active').data('num') !== undefined && $('.condition_options.active').data('clean') !== undefined && $('.type_options.active').data('room') !== undefined) {
+
         var bedrooms = Number($('.bedrooms_options.active').data('num'));
         var bathrooms = Number($('.bathrooms_options.active').data('num'));
         var cleanType = Number($('.condition_options.active').data('clean'));
         var timePerBedroom = Number($('.type_options.active').data('room'));
         var timePerBathroom = Number($('.type_options.active').data('bathroom'));
         var price =  Number($('.type_options.active').data('price'));
-        $('.section_estimator_result_block_price').text('$' + calcFunc(bathrooms, bedrooms, cleanType, timePerBedroom, timePerBathroom, price));
+
+        var ref = calcFunc(bathrooms, bedrooms, cleanType, timePerBedroom, timePerBathroom, price);
+        var hourVal = ref[0];
+        var minuteVal = ref[1];
+        var priceVal = ref[2];
+
+        $('.section_estimator_result_block_price').text('$' + priceVal);
+        if(hourVal === 0) {
+            $('.section_estimator_result_block_time').text(minuteVal +' minutes');
+        } else if (minuteVal === 0) {
+            $('.section_estimator_result_block_time').text(hourVal+' hour');
+        } else {
+            $('.section_estimator_result_block_time').text(hourVal+' hours ' + minuteVal +' minutes');
+        }
     }
 })
 
-
 var calcFunc = function (bathrooms, bedrooms, cleanType, timePerBedroom, timePerBathroom, price) {
     var totalTime = ((bedrooms * timePerBedroom) * cleanType / 100) + ((bathrooms * timePerBathroom) * cleanType / 100);
-    var totalPrice;
-    return Math.round(totalPrice = totalTime / 60 * price);
+    var totalPrice = Math.round(totalTime / 60 * price);
+    var hours;
+    var minutes;
+    (function() {
+        hours = String((totalTime / 60).toFixed(2))
+        minutes = Number((Number('0.' + hours.substr(-2)) * 60).toFixed())
+        hours =  Number(hours.slice(0, -2))
+    })();
+    return [hours, minutes, totalPrice];
 }
+
+$('.mobile_menu_wrapper').on('click', function () {
+    $('.mobile_menu, .section_header_wrapper').toggleClass('active');
+})
 
 $('.section_slider_content').slick({
   dots: true,
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 1,
-  autoplay: true,
+  // autoplay: true,
   autoplaySpeed: 3000,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 2,
         slidesToScroll: 1,
         infinite: true,
         dots: true
--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,56 @@
 $('.estimator_small_option, .estimator_big_option').on('click', (e) => {
     $(e.target).addClass('active').siblings().removeClass('active');
     if($('.bedrooms_options.active').data('num') !== undefined && $('.bathrooms_options.active').data('num') !== undefined && $('.condition_options.active').data('clean') !== undefined && $('.type_options.active').data('room') !== undefined) {
+
         let bedrooms = Number($('.bedrooms_options.active').data('num'));
         let bathrooms = Number($('.bathrooms_options.active').data('num'));
         let cleanType = Number($('.condition_options.active').data('clean'));
         let timePerBedroom = Number($('.type_options.active').data('room'));
         let timePerBathroom = Number($('.type_options.active').data('bathroom'));
         let price =  Number($('.type_options.active').data('price'));
-        $('.section_estimator_result_block_price').text('$' + calcFunc(bathrooms, bedrooms, cleanType, timePerBedroom, timePerBathroom, price));
+
+        const [hourVal, minuteVal, priceVal] = calcFunc(bathrooms, bedrooms, cleanType, timePerBedroom, timePerBathroom, price);
+
+        $('.section_estimator_result_block_price').text('$' + priceVal);
+        if(hourVal === 0) {
+            $('.section_estimator_result_block_time').text(minuteVal +' minutes');
+        } else if (minuteVal === 0) {
+            $('.section_estimator_result_block_time').text(hourVal+' hour');
+        } else {
+            $('.section_estimator_result_block_time').text(hourVal+' hours ' + minuteVal +' minutes');
+        }
     }
 })
 
-
 const calcFunc = (bathrooms, bedrooms, cleanType, timePerBedroom, timePerBathroom, price) => {
-    let totalTime = ((bedrooms * timePerBedroom) * cleanType / 100) + ((bathrooms * timePerBathroom) * cleanType / 100);
-    let totalPrice;
-    return Math.round(totalPrice = totalTime / 60 * price);
+    const totalTime = ((bedrooms * timePerBedroom) * cleanType / 100) + ((bathrooms * timePerBathroom) * cleanType / 100);
+    const totalPrice = Math.round(totalTime / 60 * price);
+    let hours;
+    let minutes;
+    (function() {
+        hours = String((totalTime / 60).toFixed(2))
+        minutes = Number((Number('0.' + hours.substr(-2)) * 60).toFixed())
+        hours =  Number(hours.slice(0, -2))
+    })();
+    return [hours, minutes, totalPrice];
 }
+
+$('.mobile_menu_wrapper').on('click', () => {
+    $('.mobile_menu, .section_header_wrapper').toggleClass('active');
+})
 
 $('.section_slider_content').slick({
   dots: true,
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 1,
-  autoplay: true,
+  // autoplay: true,
   autoplaySpeed: 3000,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 2,
         slidesToScroll: 1,
         infinite: true,
         dots: true
